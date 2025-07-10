6ffe5a90d64ef52fcaf91ecafcbb40b02e1b8da9
Engleb <engleb.a.gleb.ryzhkin@gmail.com>
2025-07-10 18:33:15 +0200

    fix of include.js

--- include.js
+++ include.js
@@ -1,7 +1,7 @@
 function includeHTML() {
     const includes = document.querySelectorAll('[data-include]');
     includes.forEach(el => {
-        const file = "../"+ el.getAttribute('data-include');
+        const file = "/SEL/"+ el.getAttribute('data-include');
         fetch(file)
             .then(response => {
                 if (!response.ok) throw new Error(`Failed to load ${file}`);
